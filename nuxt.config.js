9df4735729ffecd13f797e792684e7c01209f53f
Abhishek Sharma <abhivsharma8@gmail.com>
2021-09-04 10:18:06 +0530

    Add Meta Tags and Data
    https://nuxtjs.org/docs/2.x/features/meta-tags-seo

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,15 @@
 export default {
   components: true,
+  head: {
+    titleTemplate: "Mastering Nuxt: %s",
+    htmlAttrs: {
+      lang: "en"
+    },
+    bodyAttrs: {
+      class: ["my-style"]
+    },
+    meta: [{
+      chartset: "utf-8"
+    }]
+  }
 }

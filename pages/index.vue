b34142aeac760a676ace72b4eda3b62da5bbaab4
Abhishek Sharma <abhivsharma8@gmail.com>
2021-09-04 10:02:55 +0530

    first component and json data

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,22 @@
 <template>
-    <div> Hello Nuxt - changed !!! </div>
+    <div>
+        <div v-for="(home, index) in homes" :key="home.objectID" style="float: left; margin: 10px;">
+          <Home-Card :home="home" :index="index" />
+        </div>
+    </div>
 </template>
+<script>
+import homes from '~/data/homes';
+import HomeCard from '~/components/HomeCard';
+
+export default {
+  components: {
+    HomeCard,
+  },
+  data() {
+    return {
+      homes: homes.slice(0, 3)
+    }
+  }    
+}
+</script>
